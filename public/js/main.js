15e2f12dc8bc7d3c86b75d2f0208311ad3cc4d04
AndyLinxies <linxies@gmail.com>
2021-04-21 09:27:49 +0200

    Exo-1-Switch

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+let jour = prompt('Lundi');
+switch (jour){
+    case 'Lundi':
+    alert('Jai cours et au soir je révise')
+    break;
+    default : 
+    alert('Veuillez taper lundi')
+    break;
+}
